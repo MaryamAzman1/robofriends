82cef13eec9db100171179c0edb8dfd1e72982dd
MaryamAzman1 <maryam.azman28@gmail.com>
2023-03-17 17:11:34 +0800

    Updates

---
+++ precache-manifest.5e754084d4838ca6b25be4ecd0410ec9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "959364acac195981f6b3",
+    "url": "/robofriends/static/css/main.0ef65a35.chunk.css"
+  },
+  {
+    "revision": "959364acac195981f6b3",
+    "url": "/robofriends/static/js/main.959364ac.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "c713efef7fdd24482253",
+    "url": "/robofriends/static/css/2.f7d3a237.chunk.css"
+  },
+  {
+    "revision": "c713efef7fdd24482253",
+    "url": "/robofriends/static/js/2.c713efef.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0f176e5e6876d810658d7b37f4157945",
+    "url": "/robofriends/index.html"
+  }
+];
